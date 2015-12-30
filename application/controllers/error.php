5809532684ad3730b68ba45de1b85be03991d19c
tombayo <github@tombayo.com>
2015-12-30 17:00:09 +0100

    Update error.php
    Changed to static functions

--- application/controllers/error.php
+++ application/controllers/error.php
@@ -2,17 +2,13 @@
 
 class Error extends Controller {
 	
-	function index()
-	{
-		$this->error404();
+	public static function index() {
+	  self::error404();
 	}
 	
-	function error404()
-	{
-		echo '<h1>404 Error</h1>';
+	private static function error404() {
+    echo '<h1>404 Error</h1>';
 		echo '<p>Looks like this page doesn\'t exist</p>';
 	}
-    
-}
 
 ?>
